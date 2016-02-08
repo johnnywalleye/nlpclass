db8d68c48a4dcdaf383febea859a8d644cd4ffb7
Jon <jsondag@gmail.com>
2016-02-07 22:27:31 -0500

    JS: no 2 - unigram and bigram

--- hw2/python/LaplaceUnigramLanguageModel.py
+++ hw2/python/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,33 @@
+import collections
+import math
+
+
 class LaplaceUnigramLanguageModel:
+    def __init__(self, corpus):
+        """Initialize your data structures in the constructor."""
+        self.unigram_counts = collections.defaultdict(lambda: 0)
+        self.total = 0
+        self.num_words = 0
+        self.train(corpus)
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def train(self, corpus):
+        """ Takes a corpus and trains your language model.
+            Compute any counts or other corpus statistics in this function.
+        """
+        for sentence in corpus.corpus:
+            for datum in sentence.data:
+                token = datum.word
+                self.unigram_counts[token] += 1
+                self.total += 1
+        self.num_words = len(self.unigram_counts)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
-
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+    def score(self, sentence):
+        """ Takes a list of strings as argument and returns the log-probability of the
+            sentence using your language model. Use whatever data you computed in train() here.
+        """
+        score = 0.0
+        for token in sentence:
+            count = self.unigram_counts[token]
+            score += math.log(count + 1)
+            score -= math.log(self.total + self.num_words)
+        return score
